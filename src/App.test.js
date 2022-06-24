b9b244a98c0c2f0f7aa867ddc5e63e41c967db75
GramajoP <pablo.gramajo@proagrica.com>
2022-06-24 11:54:20 -0300

    testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,27 +1,9 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
-// import App from './App'
-import Home from './components/Pages/Home'
-// import UserContext from './contexts/UserContext'
-// import Users from './mocks/Users.mock'
-
-// afterEach(cleanup)
-
-// const renderComponent = () => {
-//   jest.mock('../Provider', () => {
-//     const React = require('react')
-//     const Store = require('../Store').default
-
-//     const StoreContext = React.createContext(new Store())
-//     return {
-//       StoreContext
-//     }
-//   })
-//   return render(<Home />)
-// }
+import App from './App'
 
 test('renders learn react link', () => {
-  render(<Home />)
-  const linkElement = screen.getByText(/learn react/i)
+  render(<App />)
+  const linkElement = screen.getByText(/hola/i)
   expect(linkElement).toBeInTheDocument()
 })
